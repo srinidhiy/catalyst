d9c4213f28f6838d610cf402597348a3e81dff0b
Maryann Xu <mxxu@usc.edu>
2023-11-20 01:31:17 -0800

    initial landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-import Image from 'next/image'
+"use client";
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
+import Banner from "./components/banner";
+//import Blog from "./components/Blog";
+import Card from "./components/Card";
+import Footer from "./components/footer";
+import Main from "./components/main";
+//import Navbar from "./components/navbar";
+import './globals.css';
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <h1>Catalyst</h1>
+    <>
+      <Main />
+      <Card />
+      <Banner />
+      <Footer />
+    </>
   )
-}
+};
+
+export default Home;
